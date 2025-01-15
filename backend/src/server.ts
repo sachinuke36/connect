23a13a95937cfcf4fa1682341a1c16d6f80d9770
sachinuke36 <sachinuke36@gmail.com>
2025-01-16 00:27:15 +0530

    frot-end-origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,12 +7,12 @@ import {app, server} from './socketHandler'
 import dotenv from'dotenv'
 dotenv.config()
 
-// const origin = process.env.NODE_ENV =="development" ?  "http://localhost:5173" : "https://connect-chat-app-pern.netlify.app"
+const origin ="https://connect-chat-app-pern.netlify.app"
 const PORT = process.env.PORT || 8000;
 
 //middlewares
 app.use(cors({
-  origin: '*',           // Allow only this specific origin
+  origin: origin,           // Allow only this specific origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
   credentials: true,
